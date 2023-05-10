7f49be8815259ad379b10a25aa6a022062739b53
Tillanueva <128622581+Tillanueva@users.noreply.github.com>
2023-05-10 07:39:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,32 @@ import sys
 import os
 from cx_Freeze import setup, Executable
 
-files = [ 'requirements.txt', 'conexion.py', 'Fondo.png', 'sort.py']
+files = ['requirements.txt', 'conexion.py', 'Fondo.png', 'sort.py']
 
 exe = Executable(script="ContadorCTP.py", base="Win32GUI")
 
-setup(
-    name="Contador de Personas | Tiendas Cortitelas",
-    version="0.1",
-    author="Tania Villanueva",
-    options={'build_exe': {'include_files': files}},
-    executables=[exe]
-)
+
+class recursion_depth:
+    def __init__(self, limit):
+        self.limit = limit
+        self.default_limit = sys.getrecursionlimit()
+
+    def __enter__(self):
+        sys.setrecursionlimit(self.limit)
+
+    def __exit__(self, type, value, traceback):
+        sys.setrecursionlimit(self.default_limit)
+
+
+def setup_():
+    setup(
+        name="Contador de Personas | Tiendas Cortitelas",
+        version="0.1",
+        author="Tania Villanueva",
+        options={'build_exe': {'include_files': files}},
+        executables=[exe]
+    )
+
+
+with recursion_depth(5000):
+    setup_()
