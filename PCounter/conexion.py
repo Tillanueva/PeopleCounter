74b12a159cf3419f725c16f065b7cbfd9629ad56
Tillanueva <128622581+Tillanueva@users.noreply.github.com>
2023-05-23 10:23:46 -0600

    Update conexion.py

--- PCounter/conexion.py
+++ PCounter/conexion.py
@@ -3,7 +3,8 @@ import os
 
 
 class conex:
-    connec = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.4.56;DATABASE=PROJECT_PC01')
+    connec = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-O6UFVI0;DATABASE=PROJECT_PC01;UID=Project01;PWD'
+                            '=PProject01')
 
     consulta = "INSERT INTO conteo(fecha, entradas) VALUES (?, ?);"
 
